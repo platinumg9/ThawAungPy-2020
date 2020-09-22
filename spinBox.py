43c5f121bb99762a634caf0c4f266a926ab90f58
Thaw Tar Aung <thinfilm.tft@gamil.com>
2020-09-22 12:48:22 +0630

    Initial Commit

---
+++ spinBox.py
@@ -0,0 +1,14 @@
+from tkinter import *
+win = Tk()
+
+win.title('SpinBar')
+win.geometry("480x320")
+
+
+spin = Spinbox(win, from_=25, to = 50, width=5)
+spin.grid(column=0, row=0)
+
+spin2 = Spinbox(win, from_=1, to = 12, width=5)
+spin2.grid(column=1, row=0)
+
+win.mainloop()
